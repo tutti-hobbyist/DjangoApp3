fba9f2d92831e7f0fd8bcfd717c479f2aa0bc3b6
tutti-hobbyist <tutti.hobbyist@gmail.com>
2022-01-08 22:39:57 +0900

    20220108_nightWork

--- todo/models.py
+++ todo/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+CHOICE = (('danger', 'high'), ('warning', 'normal'), ('primary', 'low'))
+
 class TodoModel(models.Model):
     title = models.CharField(max_length=100)
     memo = models.TextField()
+    priority = models.CharField(
+        max_length=50,
+        choices=CHOICE,
+        # null=True
+    )
+    dueDate = models.DateTimeField()
 
     def __str__(self):
         return self.title
